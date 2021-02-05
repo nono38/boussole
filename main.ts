a66f3456fd740ef88b423dbe649a700b007e8034
nono38 <74985866+nono38@users.noreply.github.com>
2021-02-05 11:03:50 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,35 +4,38 @@ input.onButtonPressed(Button.A, function () {
 })
 input.onButtonPressed(Button.B, function () {
     angle = input.compassHeading()
-    if (angle > 338 || angle < 22) {
+    if (angle >= 338 || angle <= 22) {
         basic.showString("N")
         basic.pause(100)
     } else {
-        if (angle > 158 && angle < 202) {
+        if (angle >= 158 && angle <= 202) {
             basic.showString("S")
             basic.pause(100)
         } else {
-            if (angle > 22 && angle < 67) {
+            if (angle >= 22 && angle <= 67) {
                 basic.showString("NE")
                 basic.pause(100)
             } else {
-                if (angle > 68 && angle < 112) {
+                if (angle >= 68 && angle <= 112) {
                     basic.showString("E")
                     basic.pause(100)
                 } else {
-                    if (angle > 113 && angle < 157) {
+                    if (angle >= 113 && angle <= 157) {
                         basic.showString("SE")
                         basic.pause(100)
                     } else {
-                        if (angle > 203 && angle < 247) {
+                        if (angle >= 203 && angle <= 247) {
                             basic.showString("SO")
                             basic.pause(100)
                         } else {
-                            if (angle > 248 && angle < 292) {
+                            if (angle >= 248 && angle <= 292) {
                                 basic.showString("O")
                                 basic.pause(100)
                             } else {
-                            	
+                                if (angle >= 293 && angle <= 337) {
+                                    basic.showString("NO")
+                                    basic.pause(100)
+                                }
                             }
                         }
                     }
